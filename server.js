07cfb69ed562124dae35a3b972f7f13b67d35c41
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-10-23 02:33:20 -0400

    added 404 throw to id search" git push origin mvp

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0]
+    return result
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals
     if (req.query) {
@@ -53,11 +58,15 @@ app.get('/api/animals', (req, res) => {
     res.json(results)
 });
 //param route must come after 
-app.get('/api/animals/:id', (req,res) => {
-    const results = findById(req.params.id, animals)
-    res.json(results)
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals)
+    if (result) {
+        res.json(result)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(PORT, () => {
-    console.log(`api server now on port ${PORT}`)
-})
+    console.log(`API server now on port ${PORT}!`);
+});
